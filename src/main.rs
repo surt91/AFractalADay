8576aa64ffe121fc9b9a5a46c700a7ecc000b753
Hendrik Schawe <surt91@gmail.com>
2017-05-22 23:56:34 +0200

    first test fractal works

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
+#![feature(closure_to_fn_coercion)]
+
+extern crate json;
+extern crate twitter_api as twitter;
+extern crate oauth_client as oauth;
+
+mod newton_fractal;
+
+use oauth::Token;
+use std::fs::File;
+use std::io::prelude::*;
+
+use newton_fractal::NewtonFractal;
+
+struct Auth {
+    pub consumer_key: String,
+    pub consumer_secret: String,
+    pub access_token_key: String,
+    pub access_token_secret: String
+}
+
+fn read_token(filename: &str) -> Auth {
+    let mut file = File::open("keys_and_secrets.json").unwrap();
+    let mut raw = String::new();
+    file.read_to_string(&mut raw).unwrap();
+
+    let obj = json::parse(&raw).unwrap();
+
+    Auth { consumer_key: obj["consumer_key"].to_string(),
+           consumer_secret: obj["consumer_secret"].to_string(),
+           access_token_key: obj["access_token_key"].to_string(),
+           access_token_secret: obj["access_token_secret"].to_string() }
+}
+
+fn twitter_update(auth: Auth) {
+    let consumer = Token::new(auth.consumer_key, auth.consumer_secret);
+    let access = Token::new(auth.access_token_key, auth.access_token_secret);
+
+    // twitter::update_status(&consumer, &access, &status).unwrap();
+}
+
 fn main() {
-    println!("Hello, world!");
+    let a = NewtonFractal {a: 1.,
+                           f: |z| z.powf(4.) + z.sin() + 15.,
+                           fprime: |z| 4.*z.powf(3.) + z.cos()};
+
+    a.render("test.png");
 }
---
+++ src/newton_fractal.rs
@@ -0,0 +1,106 @@
+extern crate std;
+extern crate num;
+extern crate png;
+
+use self::num::complex::Complex;
+
+use std::path::Path;
+use std::fs::File;
+use std::io::BufWriter;
+// To use encoder.set()
+use self::png::HasParameters;
+
+pub struct NewtonFractal {
+    pub a: f64,
+    pub f: fn(Complex<f64>) -> Complex<f64>,
+    pub fprime: fn(Complex<f64>) -> Complex<f64>
+}
+
+struct Convergence {
+    count: i64,
+    value: Complex<f64>
+}
+
+fn hsv2rgb(h: f64, s: f64, v: f64) -> (f64, f64, f64) {
+    // https://de.wikipedia.org/wiki/HSV-Farbraum#Umrechnung_HSV_in_RGB
+
+    let H = h * 360.;
+    let hi = (H / 60.).floor() as u32;
+    let f = H / 60. - hi as f64;
+    let p = v*(1.-s);
+    let q = v*(1.-s*f);
+    let t = v*(1.-s*(1.-f));
+
+    match hi {
+        0 => (v, t, p),
+        1 => (q, v, p),
+        2 => (p, v, t),
+        3 => (p, q, v),
+        4 => (t, p, v),
+        5 => (v, p, q),
+        6 => (v, t, p),
+        _ => (0., 0., 0.)
+    }
+}
+
+impl NewtonFractal {
+    fn iterate(&self, mut state: Complex<f64>) -> Convergence {
+        let mut ctr = 0;
+        let threshold = 1e-4;
+        let mut tmp;
+        // *attention*: this is a do while loop, mind that the "body" is actually the
+        // condition and the body is empty, thus omitted
+        while {
+            tmp = state;
+            state = state - self.a * (self.f)(state) / (self.fprime)(state);
+            ctr += 1;
+
+            (state - tmp).norm() > threshold && ctr < 10000
+        } {}
+        Convergence {count: ctr, value: state}
+    }
+
+    fn raster(&self, x: i32, y: i32, w: f64, h: f64) -> Vec<Convergence> {
+        let mut out = Vec::with_capacity((x*y) as usize);
+        for j in 0..y {
+            for i in 0..x {
+                let xp = w/x as f64 * (i-x/2) as f64;
+                let yp = h/y as f64 * (j-y/2) as f64;
+                let p = Complex {re: xp, im: yp};
+                let state = self.iterate(p);
+                out.push(state)
+            }
+        }
+        out
+    }
+
+    pub fn render(&self, filename: &str) {
+        let x = 1920;
+        let y = 1080;
+        let states = self.raster(x, y, 2., 2.);
+        let mut buffer: Vec<u8> = Vec::with_capacity((4*x*y) as usize);
+        for i in states.iter() {
+            let hue = i.value.re - i.value.re.floor();
+            let saturation = 1f64;
+            let value = if i.count as f64 / 50. > 1f64 { 1f64 } else { i.count as f64 / 50. };
+
+            let (r, g, b) = hsv2rgb(hue, saturation, value);
+            let a = 255;
+
+            buffer.push((r * 255.) as u8);
+            buffer.push((g * 255.) as u8);
+            buffer.push((b * 255.) as u8);
+            buffer.push(a);
+        }
+
+        let path = Path::new(filename);
+        let file = File::create(path).unwrap();
+        let ref mut w = BufWriter::new(file);
+
+        let mut encoder = png::Encoder::new(w, x as u32, y as u32);
+        encoder.set(png::ColorType::RGBA).set(png::BitDepth::Eight);
+        let mut writer = encoder.write_header().unwrap();
+
+        writer.write_image_data(&buffer).unwrap(); // Save
+    }
+}
