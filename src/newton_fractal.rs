2fa0694d766979c7856568e801199ca759190f5c
Hendrik Schawe <surt91@gmail.com>
2017-05-25 21:32:07 +0200

    added tests for color conversion

--- src/newton_fractal.rs
+++ src/newton_fractal.rs
@@ -3,7 +3,7 @@ extern crate num;
 extern crate png;
 extern crate rayon;
 extern crate rand;
-// extern crate log;
+extern crate test;
 
 use self::rand::Rng;
 use self::num::complex::Complex;
@@ -261,4 +261,39 @@ fn style_spooky(value: Complex<f64>, count: i64, random_color: Option<f64>, rand
     let value = 1f64.min(tmp);
 
     (hue, saturation, value)
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_hsv2rgb_red() {
+        assert_eq!((1., 0., 0.), hsv2rgb(0., 1., 1.));
+    }
+    #[test]
+    fn test_hsv2rgb_yellow() {
+        assert_eq!((1., 1., 0.), hsv2rgb(60./360., 1., 1.));
+    }
+    #[test]
+    fn test_hsv2rgb_brown() {
+        assert_eq!((0.36, 0.18, 0.09), hsv2rgb(20./360., 0.75, 0.36));
+    }
+    #[test]
+    fn test_hsv2rgb_darkgreen() {
+        assert_eq!((0., 0.5, 0.), hsv2rgb(120./360., 1., 0.5));
+    }
+    #[test]
+    fn test_hsv2rgb_orange() {
+        assert_eq!((1., 0.5, 0.), hsv2rgb(30./360., 1., 1.));
+    }
+    #[test]
+    fn test_hsv2rgb_safran() {
+        assert_eq!((1., 0.75, 0.), hsv2rgb(45./360., 1., 1.));
+    }
+    #[test]
+    fn test_hsv2rgb_indigo() {
+        assert_eq!((0.25, 0., 1.), hsv2rgb(255./360., 1., 1.));
+    }
 }
