9f801f0d619947457fe4229ee775be3256537464
Hendrik Schawe <surt91@gmail.com>
2018-02-09 17:47:15 +0100

    add default values for backward compatibility

--- src/fractal/iterated_function_system/serialize.rs
+++ src/fractal/iterated_function_system/serialize.rs
@@ -2,6 +2,18 @@ use super::NonlinearTransformation;
 use super::Transformation;
 use color::RGB;
 
+fn default_bounds() -> bool {
+    false
+}
+
+fn default_gamma() -> f64 {
+    4.0
+}
+
+fn default_vibrancy() -> f64 {
+    0.5
+}
+
 #[derive(Debug, Serialize, Deserialize)]
 pub struct IteratedFunctionSystemConfig {
     pub probabilities: Vec<f64>,
@@ -9,7 +21,10 @@ pub struct IteratedFunctionSystemConfig {
     pub transformations: Vec<Transformation>,
     pub variation: NonlinearTransformation,
     pub description: String,
+    #[serde(default = "default_bounds")]
     pub strict_bounds: bool,
+    #[serde(default = "default_gamma")]
     pub gamma: f64,
+    #[serde(default = "default_vibrancy")]
     pub vibrancy: f64
 }
