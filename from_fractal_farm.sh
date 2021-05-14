df53349c285edb23f752f27f00bed01308d99432
Hendrik Schawe <surt91@gmail.com>
2021-05-14 13:53:20 +0200

    ensure that the executable is up to date

--- from_fractal_farm.sh
+++ from_fractal_farm.sh
@@ -4,5 +4,7 @@ SERVER=localhost:7878
 JSON="json/tmp.json"
 mkdir -p json
 
+cargo build --release --features="binaries"
+
 curl $SERVER/consume > "$JSON"
 target/release/a_fractal_a_day --json $JSON -t || target/release/a_fractal_a_day -t
